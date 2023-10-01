e9b986823988e23fb3fdfb95e71077eddb364b8e
SWAPNIL  P.  KASARE <100402494+kasareswapnil09@users.noreply.github.com>
2023-10-01 18:26:27 +0530

    healthyhabits-project

--- urls.py
+++ urls.py
@@ -1,16 +1,28 @@
-# habits/urls.py
+"""
+URL configuration for healthyhabits project.
 
-from django.urls import path
-from . import views
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+# from django.conf.urls.static import static
+# from django.conf import settings
 
 urlpatterns = [
-    path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('dashboard/', views.dashboard_view, name='dashboard'),
-    path('habit/create/', views.habit_create_view, name='habit_create'),
-    path('habit/<int:habit_id>/edit/', views.habit_edit_view, name='habit_edit'),
-    path('habit/<int:habit_id>/delete/', views.habit_delete_view, name='habit_delete'),
-
-    path('track_habit/', views.track_habit, name='track_habit'),
+    path('admin/', admin.site.urls),
+    path('habits/', include('habits.urls')),
 ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
